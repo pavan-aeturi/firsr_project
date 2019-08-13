e0d392c0f1eac46f2064b474f5c90a2f9bc680b6
pavan <f20180212@goa.bits-pilani.ac.in>
2019-08-13 15:10:02 +0530

    first commit

---
+++ src/significant_Characters/significant_Characters.java
@@ -0,0 +1,9 @@
+package significant_Characters;
+public  class significant_Characters
+{
+  public static void main(String args[]) 
+           {int ia[] = new int[10];
+            System.out.println(ia);
+			System.out.println("\'\u00a0\u00a1 \u00a2 \u00a3");
+           }
+}
